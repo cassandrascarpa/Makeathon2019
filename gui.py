b545119ea509dcb1e1482503cfa2d802b7584503
cassandrascarpa <cls354@cornell.edu>
2019-02-16 14:04:55 -0500

    basic gui

--- gui.py
+++ gui.py
@@ -1,2 +1,24 @@
-import tkinter
 import serial
+from tkinter import *
+from tkinter.colorchooser import *
+
+def pickColor():
+    color = askcolor()
+
+    red = (color[0][0])/255
+    green = (color[0][1])/255
+    blue = (color[0][2])/255
+
+    k = 1 - max(red, green, blue)
+    cyan = (1 - red - k) / (1 - k)
+    magenta = (1 - green - k) / (1 - k)
+    yellow = (1 - blue - k) / (1 - k)
+
+    print("C: ", cyan, "M: ", magenta, "Y: ", yellow, "K: ", k)
+
+root = Tk()
+root.title('Color Chooser')
+screen = Canvas(root, width=800, height=800)
+screen.pack()
+Button(text='Select Color', command=pickColor).pack()
+mainloop()
